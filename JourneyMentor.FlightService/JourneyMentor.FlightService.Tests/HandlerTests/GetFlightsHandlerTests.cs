fa1254b9b90e8218203f9bb53cf0512ee0a6eac9
kevinsz8 <kevinsz2805@outlook.com>
2024-03-28 06:16:52 -0600

    Adding xUnit Test project to FlightService

---
+++ JourneyMentor.FlightService/JourneyMentor.FlightService.Tests/HandlerTests/GetFlightsHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using JourneyMentor.FlightService.Business.Handlers;
+using JourneyMentor.FlightService.Business.Messages.Query.Request;
+using JourneyMentor.FlightService.Models.Config;
+using JourneyMentor.FlightService.ServiceClients.Messages.Request;
+using JourneyMentor.FlightService.ServiceClients.Messages.Response;
+using JourneyMentor.FlightService.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyMentor.FlightService.Tests.HandlerTests
+{
+    public class GetFlightsHandlerTests
+    {
+        [Fact]
+        public async Task Handle_Success_Returns_Response()
+        {
+            var mapperMock = new Mock<IMapper>();
+            var loggerMock = new Mock<ILogger<GetFlightsHandler>>();
+            var flightInterfaceMock = new Mock<IFlightInterface>();
+            var optionsMock = new Mock<IOptions<AviationStackOptions>>();
+
+            var request = new GetFlightsHandlerRequest();
+            var cancellationToken = CancellationToken.None;
+
+            var flightResponse = new List<GetFlightsResponse>();
+
+            optionsMock.Setup(x => x.Value).Returns(new AviationStackOptions());
+
+            flightInterfaceMock.Setup(x => x.GetFlights(It.IsAny<GetFlightsRequest>()))
+                              .ReturnsAsync(flightResponse);
+
+            var handler = new GetFlightsHandler(mapperMock.Object, loggerMock.Object, flightInterfaceMock.Object, optionsMock.Object);
+
+            var result = await handler.Handle(request, cancellationToken);
+
+            Assert.NotNull(result);
+            Assert.Equal("Success", result.StatusMessage);
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public async Task Handle_Exception_Returns_ErrorResponse()
+        {
+            var mapperMock = new Mock<IMapper>();
+            var loggerMock = new Mock<ILogger<GetFlightsHandler>>();
+            var flightInterfaceMock = new Mock<IFlightInterface>();
+            var optionsMock = new Mock<IOptions<AviationStackOptions>>();
+
+            var request = new GetFlightsHandlerRequest();
+            var cancellationToken = CancellationToken.None;
+
+            optionsMock.Setup(x => x.Value).Returns(new AviationStackOptions());
+
+            flightInterfaceMock.Setup(x => x.GetFlights(It.IsAny<GetFlightsRequest>()))
+                               .ThrowsAsync(new Exception("throwing some test exception"));
+
+            var handler = new GetFlightsHandler(mapperMock.Object, loggerMock.Object, flightInterfaceMock.Object, optionsMock.Object);
+
+            var result = await handler.Handle(request, cancellationToken);
+
+            Assert.NotNull(result);
+            Assert.Equal("Error", result.StatusMessage);
+            Assert.False(result.Success);
+            Assert.Equal("An error occurred while processing your request. Please try again later.", result.ErrorMessage);
+        }
+    }
+}
---
+++ JourneyMentor.FlightService/JourneyMentor.FlightService.Tests/ServiceClientsTests/FlightInterfaceTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using JourneyMentor.FlightService.DataAccess;
+using JourneyMentor.FlightService.Models;
+using JourneyMentor.FlightService.ServiceClients;
+using JourneyMentor.FlightService.ServiceClients.Messages.Request;
+using JourneyMentor.FlightService.ServiceClients.Messages.Response;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Net;
+
+namespace JourneyMentor.FlightService.Tests.ServiceClientsTests
+{
+    public class FlightInterfaceTests
+    {
+        [Fact]
+        public async Task GetFlights_Returns_Flights()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "JourneyMentorTestDb")
+                .Options;
+            var context = new ApplicationDbContext(options);
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            var mapperMock = new Mock<IMapper>();
+            var flight = new Flight();
+
+            context.Flights.Add(flight);
+            await context.SaveChangesAsync();
+
+            var flightResponse = new List<GetFlightsResponse>();
+
+            mapperMock.Setup(x => x.Map<List<GetFlightsResponse>>(It.IsAny<List<Flight>>()))
+                      .Returns(flightResponse);
+
+            var request = new GetFlightsRequest();
+            var flightInterface = new FlightInterface(context, httpClientFactoryMock.Object.CreateClient(), mapperMock.Object);
+
+            var result = await flightInterface.GetFlights(request);
+
+            Assert.NotNull(result);
+        }
+
+    }
+}
